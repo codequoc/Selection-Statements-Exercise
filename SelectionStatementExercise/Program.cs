4639293240f65cc25ea205add27041b3609cfd65
Henry Nguyen <nguyen.henry796@gmail.com>
2022-12-08 23:41:39 -0500

    Exercise 1 done

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+            Console.WriteLine("Try to guess my favorite number");
+            var userinput = int.Parse(Console.ReadLine());
+
+            if (userinput < favNumber)
+            {
+                Console.WriteLine("too low");
+            } 
+            else if (userinput > favNumber)
+            {
+                Console.WriteLine("too high");
+            } 
+            else
+            {
+                Console.WriteLine("Nevermind");
+            }
         }
     }
 }
